61fa8f114f585886086fd48481ca71da9a000e93
kse-music <dh.hiekn@gmail.com>
2018-01-25 14:43:57 +0800

    添加设计模式

---
+++ src/test/java/com/hiekn/demo/test/design/bridge/BridgeTest.java
@@ -0,0 +1,18 @@
+package com.hiekn.demo.test.design.bridge;
+
+/**
+ * 桥接模式,使得具体类与接口实现者类无关。
+ * @author DH
+ *
+ */
+public class BridgeTest {
+	public static void main(String[] args) {
+		Shape redCircle = new Circle(100,100, 10, new ColorPrinter());
+		Shape blackCircle = new Circle(100,100, 10, new BlackPrinter());
+
+		redCircle.draw();
+		blackCircle.draw();
+	}
+}
+
+
---
+++ src/test/java/com/hiekn/demo/test/design/bridge/Printer.java
@@ -0,0 +1,5 @@
+package com.hiekn.demo.test.design.bridge;
+
+public interface Printer {
+    void print(int radius, int x, int y);
+}
