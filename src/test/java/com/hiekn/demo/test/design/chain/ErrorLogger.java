378d661e9600ce99c11a15930d3a93a7e6bd17c3
kse-music <dh.hiekn@gmail.com>
2018-01-25 14:55:17 +0800

    添加设计模式

---
+++ src/test/java/com/hiekn/demo/test/design/chain/ErrorLogger.java
@@ -0,0 +1,13 @@
+package com.hiekn.demo.test.design.chain;
+
+public class ErrorLogger extends AbstractLogger {
+
+    public ErrorLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+}
