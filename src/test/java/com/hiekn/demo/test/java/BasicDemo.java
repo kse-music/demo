0f5f4c7e86c9401686b15313a8626c8d5db39745
kse-music <dh.hiekn@gmail.com>
2018-05-10 15:04:05 +0800

    add basic demo

---
+++ src/test/java/com/hiekn/demo/test/java/BasicDemo.java
@@ -0,0 +1,13 @@
+package com.hiekn.demo.test.java;
+
+public class BasicDemo {
+    public static void main(String[] args) {
+        Integer a = 128;
+        Integer b = 128;
+        Integer c = Integer.valueOf(127);
+        Integer d = Integer.valueOf(127);
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
+        System.out.println(c == d);
+    }
+}
