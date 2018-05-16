8fff41b01677640900329388f262260811320176
DingHao <dh.hiekn@gmail.com>
2018-05-16 22:29:46 +0800

    add Java beans example

--- src/test/java/com/hiekn/demo/test/java/BasicDemo.java
+++ src/test/java/com/hiekn/demo/test/java/BasicDemo.java
@@ -1,7 +1,13 @@
 package com.hiekn.demo.test.java;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.core.MethodParameter;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+
 public class BasicDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Integer a = 128;
         Integer b = 128;
         Integer c = Integer.valueOf(127);
@@ -9,5 +15,30 @@ public class BasicDemo {
         System.out.println(a == b);
         System.out.println(a.equals(b));
         System.out.println(c == d);
+        PropertyDescriptor[] descriptors = BeanUtils.getPropertyDescriptors(P.class);
+        P p = new P();
+        for (PropertyDescriptor descriptor : descriptors) {
+            String name = descriptor.getName();
+            if(!"class".equals(name)){
+                MethodParameter methodDescriptors = BeanUtils.getWriteMethodParameter(descriptor);
+                System.out.println(name);
+                Method method = methodDescriptors.getMethod();
+                method.invoke(p,1);
+            }
+        }
+        System.out.println(p.getA());
     }
+}
+
+class P{
+    private Integer a;
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
 }
