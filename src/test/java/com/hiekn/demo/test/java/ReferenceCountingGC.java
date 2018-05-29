f6426e834a83d1f46358f229766d34d16eef5217
kse-music <dh.hiekn@gmail.com>
2018-05-29 17:03:32 +0800

    add gc example

---
+++ src/test/java/com/hiekn/demo/test/java/ReferenceCountingGC.java
@@ -0,0 +1,23 @@
+package com.hiekn.demo.test.java;
+
+public class ReferenceCountingGC {
+
+    private Object instance;
+
+    private static final int _1MB = 1024 *1024;
+
+    private byte[] bigSize = new byte[2 * _1MB];
+
+
+    public static void main(String[] args) {
+        ReferenceCountingGC objA = new ReferenceCountingGC();
+        ReferenceCountingGC objB = new ReferenceCountingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+
+        objA = null;
+        objB = null;
+        System.gc();
+    }
+}
+
