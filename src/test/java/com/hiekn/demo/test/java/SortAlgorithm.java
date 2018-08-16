d0a731e56d7a4dda3c4bdfc91113962a2cc23a7f
kse-music <dh.hiekn@gmail.com>
2018-08-16 18:52:31 +0800

    add quickSort demo

--- src/test/java/com/hiekn/demo/test/java/SortAlgorithm.java
+++ src/test/java/com/hiekn/demo/test/java/SortAlgorithm.java
@@ -7,37 +7,51 @@ public class SortAlgorithm {
 		int[] arr = {77,99,44,55,22,88,11,0,66,33,1};
 //		bubbleSort(arr);
 //		selectSort(arr);
-		insertSort(arr);
+//		insertSort(arr);
+        quickSort(arr,0,arr.length-1);
 		System.out.println(Arrays.toString(arr));
 	}
-	
+
+    /**
+     * 找到最小数就交换位置,稳定
+     * @param arr
+     */
 	public static void bubbleSort(int[] arr){
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
 				if(arr[i] > arr[j]){
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
+                    swap(arr,i,j);
+                }
 			}
 		}
 	}
-	
+
+    /**
+     * 找到最小数的小标，最后交换一次位置，不稳定
+     * @param arr
+     */
 	public static void selectSort(int[] arr){
-		int min = 0;
 		for (int i = 0; i < arr.length - 1; i++) {
-			min = i;
-			for (int j = i + 1; j < arr.length; j++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
 				if(arr[min] > arr[j]){
 					min = j;
 				}
 			}
-			int temp = arr[i];
-			arr[i] = arr[min];
-			arr[min] = temp;
+            swap(arr,i,min);
 		}
 	}
-	
+
+	private static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * 稳定
+     * @param arr
+     */
 	public static void insertSort(int[] arr){
 		int i, j, insertNote;// 要插入的数据
         for (i = 1; i < arr.length; i++) {// 从数组的第二个元素开始循环将数组中的元素插入
@@ -50,4 +64,32 @@ public class SortAlgorithm {
             arr[j + 1] = insertNote;// 直到要插入的元素不小于第j个元素,将insertNote插入到数组中
         }
 	}
+
+    /**
+     * 不稳定
+     * @param arr
+     * @param low
+     * @param high
+     */
+    public static void quickSort(int[] arr, int low, int high) {// 传入low=0，high=array.length-1;
+        int pivot, p_pos, i, t;// pivot->位索引;p_pos->轴值。
+        if (low < high) {
+            p_pos = low;
+            pivot = arr[p_pos];
+            for (i = low + 1; i <= high; i++)
+                if (arr[i] < pivot) {
+                    p_pos++;
+                    t = arr[p_pos];
+                    arr[p_pos] = arr[i];
+                    arr[i] = t;
+                }
+            t = arr[low];
+            arr[low] = arr[p_pos];
+            arr[p_pos] = t;
+            // 分而治之
+            quickSort(arr, low, p_pos - 1);// 排序左半部分
+            quickSort(arr, p_pos + 1, high);// 排序右半部分
+        }
+    }
+
 }
