5c1424af18529a87bf3a82c80a4f18a202a6ed33
kse-music <dh.hiekn@gmail.com>
2018-09-04 15:05:41 +0800

    add http demo

--- src/test/java/com/hiekn/demo/test/HttpUtilsTest.java
+++ src/test/java/com/hiekn/demo/test/HttpUtilsTest.java
@@ -1,15 +1,16 @@
 package com.hiekn.demo.test;
 
-import com.hiekn.demo.bean.Student;
-import com.hiekn.demo.bean.result.RestResp;
-import com.hiekn.demo.util.HttpUtils;
-
-import javax.ws.rs.core.GenericType;
+import com.hiekn.demo.util.HttpRequest;
 
 public class HttpUtilsTest {
 	public static void main(String[] args) {
-		String url = "http://192.168.1.119:9080/demo/api/test/test2";
-		RestResp<Student> rs = HttpUtils.sendPost(url, null, null, new GenericType<RestResp<Student>>(){});
-		System.out.println(rs.getData().getAge());
+        testHttp();
 	}
+
+
+	private static void testHttp(){
+        String s = HttpRequest.sendGet("http://www.hiboot.cn/");
+        System.out.println(s);
+    }
+
 }
