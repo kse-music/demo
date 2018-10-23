e93c45a019a5816ca7c97f65f22199df3975b8ef
kse-music <dh.hiekn@gmail.com>
2018-10-23 22:34:36 +0800

    an

--- src/test/java/com/hiekn/demo/test/java/generic/GenericDemo.java
+++ src/test/java/com/hiekn/demo/test/java/generic/GenericDemo.java
@@ -4,7 +4,34 @@ public class GenericDemo {
 
     public static void main(String[] args) {
 
+        new A().a();
+
+        A a = new A() {
+            @Override
+            public void a() {
+                System.out.println("over a");
+            }
+
+            @Override
+            public void b() {
+                System.out.println("over b");
+
+            }
+        };
+        a.a();
+
     }
 
 
+}
+
+class A{
+
+    public void a(){
+        System.out.println("a");
+    }
+
+    public void b(){
+        System.out.println("b");
+    }
 }
