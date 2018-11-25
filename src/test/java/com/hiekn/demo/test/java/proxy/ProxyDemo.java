d7855fb16e540c77959fc1fbf765b597ae512a2d
kse-music <dh.hiekn@gmail.com>
2018-11-25 22:54:36 +0800

    add asm

--- src/test/java/com/hiekn/demo/test/java/proxy/ProxyDemo.java
+++ src/test/java/com/hiekn/demo/test/java/proxy/ProxyDemo.java
@@ -5,8 +5,13 @@ import com.hiekn.demo.test.java.proxy.cglib.MyMethodInterceptor;
 import com.hiekn.demo.test.java.proxy.jdk.Hello;
 import com.hiekn.demo.test.java.proxy.jdk.HelloImpl;
 import com.hiekn.demo.test.java.proxy.jdk.LogInvocationHandler;
+import org.springframework.asm.ClassWriter;
+import org.springframework.asm.MethodVisitor;
+import org.springframework.asm.Opcodes;
 import org.springframework.cglib.proxy.Enhancer;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.lang.reflect.Proxy;
 
 public class ProxyDemo {
@@ -25,7 +30,48 @@ public class ProxyDemo {
 
         HelloConcrete hello2 = (HelloConcrete)enhancer.create();
         System.out.println(hello2.sayHello("I love you!"));
+        try {
+            helloWorld();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    public static void helloWorld() throws Exception {
+        ClassWriter classWriter = new ClassWriter(0);
+        String className = "com/hiekn/asm/HelloWorld";
+        classWriter.visit(Opcodes.V1_5, Opcodes.ACC_PUBLIC, className, null,
+                "java/lang/Object", null);
+
+        MethodVisitor initVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "<init>",
+                "()V", null, null);
+        initVisitor.visitCode();
+        initVisitor.visitVarInsn(Opcodes.ALOAD, 0);
+        initVisitor.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>",
+                "V()");
+        initVisitor.visitInsn(Opcodes.RETURN);
+        initVisitor.visitMaxs(1, 1);
+        initVisitor.visitEnd();
+
+        MethodVisitor helloVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "sayHello",
+                "()V;", null, null);
+        helloVisitor.visitCode();
+        helloVisitor.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out",
+                "Ljava/io/PrintStream;");
+        helloVisitor.visitLdcInsn("hello world!");
+        helloVisitor.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream",
+                "println", "(Ljava/lang/String;)V");
+        helloVisitor.visitInsn(Opcodes.RETURN);
+        helloVisitor.visitMaxs(1, 1);
+        helloVisitor.visitEnd();
+
+        classWriter.visitEnd();
+        byte[] code = classWriter.toByteArray();
+        File file = new File("D:\\HelloWorld.class");
+        FileOutputStream output = new FileOutputStream(file);
+        output.write(code);
+        output.close();
     }
 
 }
