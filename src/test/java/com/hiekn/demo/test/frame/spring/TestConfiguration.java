df7c01f95640337517f2b6da06271700cb6dca7a
kse-music <dh.hiekn@gmail.com>
2018-11-26 15:35:37 +0800

    research spring

--- src/test/java/com/hiekn/demo/test/frame/spring/TestConfiguration.java
+++ src/test/java/com/hiekn/demo/test/frame/spring/TestConfiguration.java
@@ -1,17 +1,30 @@
 package com.hiekn.demo.test.frame.spring;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
 
 //@Configuration
 //@ComponentScan("com.hiekn.demo.test.frame.spring")
-public class TestConfiguration {
+public class TestConfiguration implements InitializingBean {
 
     @Autowired
     private TestConfiguration a;
 
     public TestConfiguration(){
         System.out.println(a);
-        System.out.println("TestConfiguration");
+        System.out.println("Constructor");
+    }
+
+    @PostConstruct
+    private void init(){
+        System.out.println("@PostConstruct");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
     }
 
 //    @Bean注解注册bean,同时可以指定初始化和销毁方法
