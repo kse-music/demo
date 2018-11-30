76ee7e439d1ac9c2d3cd05dd401a7677867ffe1b
kse-music <dh.hiekn@gmail.com>
2018-11-30 18:56:19 +0800

    research mybatis

--- src/test/java/com/hiekn/demo/test/frame/spring/SpringDemo.java
+++ src/test/java/com/hiekn/demo/test/frame/spring/SpringDemo.java
@@ -1,32 +1,34 @@
 package com.hiekn.demo.test.frame.spring;
 
+import com.hiekn.demo.test.TestBase;
 import com.hiekn.demo.test.frame.spring.aop.AopConfig;
-import org.springframework.beans.factory.InitializingBean;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class SpringDemo {
+public class SpringDemo extends TestBase {
 
-    public static void main(String[] args) {
+    @Test
+    public void ioc() {
 
         //@Configuration注解的spring容器加载方式，用AnnotationConfigApplicationContext替换ClassPathXmlApplicationContext
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.hiekn.demo.test.frame.spring");
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.hiekn.demo.test.frame.spring");
 
         //如果加载spring-context.xml文件：
 //        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
 
-//        TestBean tb = context.getBean(TestBean.class);
-//        System.out.println(tb.getName());
-//        DemoBean1 demoBean1 = context.getBean(DemoBean1.class);
-//        System.out.println(demoBean1);
-//        demoBean1.d();
-//        System.out.println(context.getBean(DemoBean2.class));
+        TestBean tb = context.getBean(TestBean.class);
+        System.out.println(tb.getName());
+        DemoBean1 demoBean1 = context.getBean(DemoBean1.class);
+        System.out.println(demoBean1);
+        demoBean1.d();
+        System.out.println(context.getBean(DemoBean2.class));
 
-        justOne();
 
     }
 
-    public static void justOne(){
+    @Test
+    public void justOne(){
         ApplicationContext context = new AnnotationConfigApplicationContext(TestConfiguration.class,AopConfig.class);
         TestConfiguration bean = context.getBean(TestConfiguration.class);
         System.out.println(bean.test());
