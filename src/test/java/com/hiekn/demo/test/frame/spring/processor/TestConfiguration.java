10bcf3aa2356b6fd9501f5fec3b3306784bad2fc
kse-music <dh.hiekn@gmail.com>
2018-12-20 10:58:46 +0800

    ImportResource

--- src/test/java/com/hiekn/demo/test/frame/spring/processor/TestConfiguration.java
+++ src/test/java/com/hiekn/demo/test/frame/spring/processor/TestConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import javax.annotation.PostConstruct;
 
 @ComponentScan("com.hiekn.demo.test.frame.spring.processor")
+//@ImportResource("classpath:spring-config.xml")
 public class TestConfiguration implements InitializingBean {
 
     @Value("${user.home}")
