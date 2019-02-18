5955dbc39f4f43d45feecfa13d891fd41d4d89c1
kse-music <dh.hiekn@gmail.com>
2019-02-18 13:33:13 +0800

    推荐使用构造函数注入

--- src/test/java/com/hiekn/demo/test/frame/spring/processor/SingleResearchSpring.java
+++ src/test/java/com/hiekn/demo/test/frame/spring/processor/SingleResearchSpring.java
@@ -2,7 +2,6 @@ package com.hiekn.demo.test.frame.spring.processor;
 
 import com.hiekn.demo.test.frame.spring.basic.ExampleBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,15 +15,28 @@ public class SingleResearchSpring implements InitializingBean {
     @Value("${user.home2}")
     private String home;
 
-    @Autowired
+//    @Autowired
     private ExampleBean exampleBean;
 
     public void setHome(String home) {
         this.home = home;
     }
 
-    public SingleResearchSpring(){
-        System.out.println("Constructor invoke");
+
+//    public SingleResearchSpring(){
+//        System.out.println("Constructor invoke");
+//    }
+
+    /**
+     * 10、推荐使用构造函数注入
+     *
+     * 这一条实践来自Phil Webb（Spring Boot的项目负责人, @phillip_webb）。
+     *
+     * 保持业务逻辑免受Spring Boot代码侵入的一种方法是使用构造函数注入。
+     * 不仅是因为@Autowired注解在构造函数上是可选的，而且还可以在没有Spring的情况下轻松实例化bean。
+     */
+    public SingleResearchSpring(ExampleBean exampleBean) {
+        this.exampleBean = exampleBean;
     }
 
     @PostConstruct
