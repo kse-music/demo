24001d37c90c2f382355bb1799d6493bd322f8e4
kse-music <dh.hiekn@gmail.com>
2019-03-20 16:26:57 +0800

    字符串转整数

--- src/test/java/com/hiekn/demo/test/algorithm/AlgorithmDemo.java
+++ src/test/java/com/hiekn/demo/test/algorithm/AlgorithmDemo.java
@@ -22,6 +22,45 @@ public class AlgorithmDemo extends TestBase {
         System.out.println(Arrays.toString(arr));
     }
 
+    @Test
+    public void sti() {
+        System.out.println(myAtoi("-91283472332"));
+    }
+
+    public int myAtoi(String str) {
+        int n = str.length();
+        int i = 0;
+        while(i < n && str.charAt(i) == ' ') {
+            i++;
+        }
+        if(i == n || !((str.charAt(i) == '+') || (str.charAt(i) == '-') ||(str.charAt(i) >= '0' && str.charAt(i) <= '9'))) {
+            return 0;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        if(str.charAt(i) == '-') {
+            stringBuilder.append('-');
+            i++;
+        }else if(str.charAt(i) == '+') {
+            i++;
+        }
+        if(i == n || !(str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+            return 0;
+        }
+        while(i < n && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            stringBuilder.append(str.charAt(i));
+            i++;
+        }
+        try {
+            return Integer.valueOf(stringBuilder.toString());
+        }catch (Exception e) {
+            if(stringBuilder.substring(0, 1).equals("-")) {
+                return Integer.MIN_VALUE;
+            }else {
+                return Integer.MAX_VALUE;
+            }
+        }
+    }
+
 
     @Test
     public void computeAdjacentWords(){
