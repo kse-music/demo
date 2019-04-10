52417069d6b9971a1ec6bc4c094b5f3e4d390b2c
kse-music <dh.hiekn@gmail.com>
2019-04-11 00:05:06 +0800

    remove some frame demo

--- src/test/java/com/hiekn/demo/test/java/junior/JavaDemo.java
+++ src/test/java/com/hiekn/demo/test/java/junior/JavaDemo.java
@@ -27,13 +27,6 @@ import java.util.Date;
 import java.util.Map;
 import java.util.Properties;
 
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.JavascriptExecutor;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-
-
 /**
  * 综合性示例，各种小demo在此
  *
@@ -173,34 +166,5 @@ public class JavaDemo extends TestBase {
 
         claims.forEach((k, v) -> System.out.println(k + "  " + v.asString()));
     }
-
-    //	private static WebDriver driver;
-//
-//	private static final String URL = "http://news.sohu.com/20170320/n483936679.shtml";
-////	private static final String URL = "https://www.crunchbase.com/organization/microsoft/people";
-//
-//	public static void main(String[] args) throws Exception {
-//      String URL = "http://news.sohu.com/20170320/n483936679.shtml";
-//      String URL = "https://www.crunchbase.com/organization/microsoft/people";
-//		System.setProperty("webdriver.gecko.driver", "driver/geckodriver.exe");
-//
-//		WebDriver driver = new FirefoxDriver();
-////		driver.manage().window().maximize();
-//		driver.get(URL);
-//
-//		Thread.sleep(2000);
-//		((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-//		Thread.sleep(1000);
-////		List<WebElement> eles = driver.findElements(By.cssSelector(".section-list li .info-block h4 a"));
-//		System.out.println("find");
-//		List<WebElement> eles = driver.findElements(By.cssSelector("div.content-box h1"));
-//		System.out.println(eles.size()+"条数据");
-//		for (WebElement e : eles) {
-//			System.out.println(e.getText());
-//		}
-//
-//		System.out.println("finish");
-//	}
-
 
 }
